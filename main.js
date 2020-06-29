573ac8ce44745f97c5ffdb1071cddad0c311429f
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-06-29 13:51:31 -0300

    Webpack dev server

--- main.js
+++
@@ -1,7 +0,0 @@
-import { soma, multiplicacao as multiplica } from './funcoes'; // export devemos chamar o mesmo nome da função
-import subtrair from './subtracao';// no export defaul podemos chamar com o nome que a gente quiser
-// import * as funcoes './funcoes'; // Importa todas as funções em um objeto
-
-console.log(soma(1, 2));
-console.log(subtrair(8, 3));
-console.log(multiplica(5, 5)); // Renomeando a função importada
