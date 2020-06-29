71c7bf2282e236fcd821e559612a9bcc012fac06
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-06-29 14:18:19 -0300

    Axios com Async e Await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-alert("OPA");
+import axios from 'axios'; // Todas as funções do axios retoram promise
+
+class Api {
+    static async getUserInfo(username) {
+        try {
+            const response = await axios.get(`https://api.github.com/users/${username}`);            
+            console.log(response);
+        } catch (error) {
+            console.warn('Erro na API');
+        }
+    }
+}
+
+
+Api.getUserInfo('andresilvadev');
